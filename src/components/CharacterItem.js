6680c33ce2e786a4f3111a03d6945908c748ba84
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-28 18:38:46 -0700

    test now passes with string interpolation

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -2,10 +2,11 @@ import Component from './Component.js';
 
 class CharacterItem extends Component {
     renderTemplate() {
+        const character = this.props.character;
         return /*html*/ `
             <li>
-                <h2>Gombo</h2>
-                <img src="https://vignette.wikia.nocookie.net/avatar/images/b/b2/Gombo.png/revision/latest?cb=20140629232813">
+                <h2>${character.name}</h2>
+                <img src="${character.photoUrl}">
             </li>
         `;
     }
