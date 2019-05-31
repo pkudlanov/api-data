1bb5bb1b87bfff16384be1bac6686539e937dc63
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-31 13:34:25 -0700

    got enemies/allies message to appear correctly

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,35 +5,25 @@ class Header extends Component {
         const charsLength = this.props.characters.length;
 
         const message = this.props.message;
-        //const capitalized = message.charAt(0).toUpperCase() + message.slice(1);
 
         const wholeMessage = message.split('=', 2);
         
-        console.log(message, 'message');
-         
         const frenemy = wholeMessage[0];
         let name = wholeMessage[1];
         
-        console.log(frenemy, 'fren');
+        if(name){
+            name = name.replace(/%20/g, ' ');
+        }
         
-        // console.log(nameRaw, 'nameRaw');
-        
-        
-        
-        // if(name){
-        //     name = name.replace(/%20/g, ' ');
-        //     console.log(name, 'name');
-        // }
-        
-        // let finalMessage;
-        
-        // if(!charsLength && !message){
-        //     finalMessage = 'All the Characters';
-        // } else if(!message){
-        //     finalMessage = 'All the characters';
-        // } else {
-        //     finalMessage = name + ' has ' + charsLength + ' ' + frenemy + '.';
-        // }
+        let finalMessage;
+
+        if(!message){
+            finalMessage = 'All the characters';
+        } else if(!charsLength){
+            finalMessage = name + ' has no ' + frenemy;
+        } else {
+            finalMessage = name + ' has ' + charsLength + ' ' + frenemy + '.';
+        }
 
         return /*html*/ `
             <header>
